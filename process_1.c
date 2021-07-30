6a53732d0065ec13ba405c85a6eede4fcec9c3fd
vinayak_koshti <vinayakkoshti111@gmail.com>
2021-07-30 12:19:18 +0530

    updated

--- process_1.c
+++ process_1.c
@@ -1,3 +1,10 @@
+/*
+2. Open the file /usr/share/dictionary in process P1 and 
+send the strings starting with letter 'm' to process P2 using share memory.
+ In process P2 find and print the length of string.
+*/
+
+
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>   
@@ -14,14 +21,11 @@ int main()
     int num;
     
     shmd=shm_open("/vi",O_RDWR | O_CREAT,S_IRWXU);
-    ftruncate(shmd,sizeof(char));
+    ftruncate(shmd,sizeof(buff));
 
-    count=(char*)mmap(NULL,sizeof(char),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0);
+    count=(char*)mmap(NULL,sizeof(buff),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0);
 
-   
-   
-
-      strcpy(count,buff);
+    strcpy(count,buff);
 
     return 0;
 }
--- process_2.c
+++ process_2.c
@@ -1,3 +1,4 @@
+
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>   
@@ -6,7 +7,7 @@
 
 #include <fcntl.h>    
 #include <sys/stat.h> 
-#include <semaphore.h>
+
 #include<string.h>
 
 char *count;
@@ -15,19 +16,18 @@ ssize_t readq;
 
 int main()
 {
-    int shmd;
+    int shmd; //discriptor 
     
     shmd=shm_open("/vi",O_RDWR | O_CREAT,S_IRWXU);
     ftruncate(shmd,sizeof(buff));
 
-    count=(char*)mmap(NULL,sizeof(int),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0);
+    count=(char*)mmap(NULL,sizeof(int),PROT_READ | PROT_WRITE,MAP_SHARED,shmd,0); //map memory 
     
 
         printf("string : %s\n",count);
        // readq=read(shmd,buff,sizeof(buff));
         strcpy(buff,count);
     
-
         printf("length of msg : %ld\n",strlen(buff));
 
               
